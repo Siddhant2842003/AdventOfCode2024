6e14cd268936ee82a3956e4b26256cd07f133edc
Siddhant Mittal <siddhantmittal@Siddhants-MacBook-Air.local>
2025-01-19 20:15:11 -0500

    completed problem 3

--- problems/src/main.rs
+++ problems/src/main.rs
@@ -1,9 +1,10 @@
 mod problem1;
 mod problem2;
+mod problem3;
 
 fn main() {
     println!("Hello, world!");
     // println!("{}",problem1::problem1::solve());
-    println!("{}",problem2::problem2::solve());
-
+    // println!("{}",problem2::problem2::solve());
+    println!("{}", problem3::problem3::solve());
 }
